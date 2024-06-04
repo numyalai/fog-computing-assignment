38dd42ae403fc077e4e665064c50299a426ab27c
Sultan Numyalai <sultannumyalai@Sultans-MBP.fritz.box>
2024-06-05 00:30:51 +0300

    routes directory

---
+++ services/routes.go
@@ -0,0 +1,13 @@
+package services
+
+import "net/http"
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+var Routes = [...] Route{
+}
