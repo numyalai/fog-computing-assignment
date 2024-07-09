0cc4b81add97d37bd46f69600555312cba3754bb
Niklas Kowallik <nkowallix+git@gmail.com>
2024-07-10 00:13:17 +0200

    make help scripts executable for localhost

--- requesting_manageable.sh
+++ requesting_manageable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [ true ]; do
-    curl -X POST 34.65.17.116:6001 --data "TEST"
+    curl -X POST localhost:6001 --data "TEST"
     sleep 8
 done
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 trap 'kill $(jobs -p)' SIGINT
-#./services/router &
+./services/router &
 ./services/client 34.65.17.116:5001 &
 ./services/cpu_watcher &
 ./services/ram_watcher &
